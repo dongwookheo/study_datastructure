aae05134365d239a3b6f2f44c36817de26337133
dongwookheo <hdwook3918@gmail.com>
2024-02-29 12:35:01 +0900

    Added test with stack class

---
+++ data_structure/test/stack_test.py
@@ -0,0 +1,11 @@
+from data_structure import stack
+
+if __name__ == '__main__':
+    s = stack.Stack(100)
+    msg = input("Enter message: ")
+    for char in msg:
+        s.push(char)
+
+    print("Reversed message: ", end='')
+    while not s.is_empty():
+        print(s.pop(), end='')
