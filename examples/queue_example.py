405d1818bb6f2b0a8672e4628293615edcca89ea
dongwookheo <hdwook3918@gmail.com>
2024-03-20 23:51:16 +0900

    Add queue example with Queue

--- examples/queue_example.py
+++ examples/queue_example.py
@@ -1,2 +1,22 @@
 import queue
+import random
 
+
+def queue_using_Queue():
+    q = queue.Queue(20)
+
+    print("Order of insertion: ")
+    while not q.full():
+        value = random.randint(0, 100)
+        q.put(value)
+        print(value, end=' ')
+    print()
+
+    print("Order of deletion: ")
+    while not q.empty():
+        print(q.get(), end=' ')
+    print()
+
+
+if __name__ == '__main__':
+    queue_using_Queue()
